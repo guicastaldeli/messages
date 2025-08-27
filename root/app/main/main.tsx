c6c8b8a3e060cedb457ce3b6adfc471ce39c8de4
gui castaldeli <guicastaldeli8@gmail.com>
2025-08-27 09:56:21 -0400

    update-00

---
+++ root/app/main/main.tsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import './_styles/styles.scss';
+
+export class Main extends Component {
+    constructor(props: any) {
+        super(props);
+        this.render();
+    }
+
+    public render() {
+        return (
+            <div id='main'>
+                main32323asdasdasdasdasdasdasdasdsdad
+            </div>
+        )
+    }
+}
