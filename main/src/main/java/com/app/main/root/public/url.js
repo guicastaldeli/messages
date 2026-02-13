131fe6900a09eb3857e8da3ed7018f7ea880b2a4
gui castaldeli <guicastaldeli8@gmail.com>
2026-02-13 03:14:13 -0400

    u

---
+++ main/src/main/java/com/app/main/root/public/url.js
@@ -0,0 +1,39 @@
+// Environment Configuration
+window.APP_ENV = 'prod';
+window.LOAD_BALANCER_ENABLED = true;
+window.SERVER_INSTANCES = ['server1:3001', 'server2:3003', 'server3:3004'];
+window.HEALTH_CHECK_INTERVAL = 30000;
+window.MAX_FAILURE_COUNT = 3;
+
+// Encryption
+window.ENCRYPTION_MASTER_KEY = 'x63uuphvQo1qCK4Y3kh2f77iRVwnUtXckI+eeUJgGng=';
+window.RESET_TOKEN = 'F5h+fKbh04s/+nvJKOM1Av7b8JQ3KmBHCUjpWzoE1RE=';
+
+// Web URLs
+window.WEB_URL = 'https://messages-psi-teal.vercel.app';
+window.NEXT_PUBLIC_WEB_URL = 'https://messages-psi-teal.vercel.app';
+
+// Server Configuration
+window.MAIN_SERVER_ID = 'xFGVJr+VaQSt9RHIBpNlazxAf';
+window.DEFAULT_URL = 'http://messages-server-instance.eba-yvhbyvad.us-west-2.elasticbeanstalk.com:';
+window.SERVER_URL = 'http://messages-server-instance.eba-yvhbyvad.us-west-2.elasticbeanstalk.com';
+window.SERVER_ALT_URL = 'http://messages-server-instance.eba-yvhbyvad.us-west-2.elasticbeanstalk.com';
+
+// Server HTTP Endpoints
+window.SERVER_DEF_HTTP_URL = 'http://messages-server-instance.eba-yvhbyvad.us-west-2.elasticbeanstalk.com/main';
+window.SERVER_DIR_HTTP_URL = 'http://messages-server-instance.eba-yvhbyvad.us-west-2.elasticbeanstalk.com/main/direct';
+
+// Server WebSocket Endpoints
+window.SERVER_DEF_WS_URL = 'ws://messages-server-instance.eba-yvhbyvad.us-west-2.elasticbeanstalk.com/main';
+window.SERVER_DIR_WS_URL = 'ws://messages-server-instance.eba-yvhbyvad.us-west-2.elasticbeanstalk.com/main/direct';
+
+// Public Server URLs
+window.NEXT_PUBLIC_SERVER_ALT_URL = 'http://messages-server-instance.eba-yvhbyvad.us-west-2.elasticbeanstalk.com';
+window.NEXT_PUBLIC_SERVER_DEF_HTTP_URL = 'http://messages-server-instance.eba-yvhbyvad.us-west-2.elasticbeanstalk.com/main';
+window.NEXT_PUBLIC_SERVER_DIR_HTTP_URL = 'http://messages-server-instance.eba-yvhbyvad.us-west-2.elasticbeanstalk.com/main/direct';
+window.NEXT_PUBLIC_SERVER_DEF_WS_URL = 'ws://messages-server-instance.eba-yvhbyvad.us-west-2.elasticbeanstalk.com/main';
+window.NEXT_PUBLIC_SERVER_DIR_WS_URL = 'ws://messages-server-instance.eba-yvhbyvad.us-west-2.elasticbeanstalk.com/main/direct';
+
+// API URLs
+window.API_URL = 'http://messages-api-instance.eba-w4qijkkd.us-west-2.elasticbeanstalk.com';
+window.NEXT_PUBLIC_API_URL = 'http://messages-api-instance.eba-w4qijkkd.us-west-2.elasticbeanstalk.com';
