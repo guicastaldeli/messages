f64b9ab712477e695b2e901d3c853fe86a39f80a
gui castaldeli <guicastaldeli8@gmail.com>
2026-02-13 20:18:40 -0400

    updatee

--- main/src/main/java/com/app/main/root/app/layout.tsx
+++ main/src/main/java/com/app/main/root/app/layout.tsx
@@ -28,9 +28,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.png" type="image/png" />
-      </head>
       <body 
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
